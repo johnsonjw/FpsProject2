800b6ccd54288f9bb4df4a1176c93e1d83fea7ed
johnsonjw <46804126+johnsonjw@users.noreply.github.com>
2019-02-21 16:57:16 -0600

    fixing stuff

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -17,20 +17,18 @@ public class Gun : MonoBehaviour
         {
             Shoot();
         }
-        if (Input.GetMouseButtonDown(0))
-        {
-            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            go.transform.position = Camera.main.transform.position;
-            Rigidbody rb = go.AddComponent<Rigidbody>();
-            Vector3 v3T = Input.mousePosition;
-            v3T.z = 10.0f;
-            go.transform.LookAt(Camera.main.ScreenToWorldPoint(v3T));
-            rb.AddRelativeForce(Vector3.forward * 1000);
-        }
+      
     }
 
     public void Shoot()
     {
+        GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        go.transform.position = Camera.main.transform.position;
+        Rigidbody rb = go.AddComponent<Rigidbody>();
+        Vector3 v3T = Input.mousePosition;
+        v3T.z = 10.0f;
+        go.transform.LookAt(Camera.main.ScreenToWorldPoint(v3T));
+        rb.AddRelativeForce(Vector3.forward * 1000);
         RaycastHit hit;
 
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
